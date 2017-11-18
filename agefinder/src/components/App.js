275218ca4cd8101d08c972e1c1e59849ad68db0d
Zeydi <oussema.zeidi@ieee.org>
2017-11-18 16:48:03 +0100

    added age stats

--- agefinder/src/components/App.js
+++ agefinder/src/components/App.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
+import AgeStats from './AgeStats';
+
 class  App extends Component {
+  constructor() {
+    super();
+
+    this.state= {
+      newDate: '',
+      birthday: '1991-10-13'
+    }
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({ birthday: this.state.newDate });
+  }
   render() {
     return (
       <div className="App" >
         <Form inline>
           <h2>Input Your Birthday !</h2>
-          <FormControl type= "date">
+          <FormControl
+            type= "date"
+            onChange={ event => this.setState({ newDate: event.target.value })}
+          >
           </FormControl>
           {' '}
-          <Button>
+          <Button onClick={() => this.changeBirthday()}>
             Submit
           </Button>
+          <AgeStats date={this.state.birthday}/>
         </Form>
       </div>
     )
