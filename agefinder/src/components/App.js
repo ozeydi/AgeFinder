ebc2928868a372f08fb3611fc4c928e0be1be837
Zeydi <oussema.zeidi@ieee.org>
2017-11-18 16:05:59 +0100

    added form to App component

--- agefinder/src/components/App.js
+++ agefinder/src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css';
 
 class  App extends Component {
   render() {
     return (
-      <div>agefinder component</div>
+      <div className="App" >
+        <Form inline>
+          <h2>Input Your Birthday !</h2>
+          <FormControl type= "date">
+          </FormControl>
+          {' '}
+          <Button>
+            Submit
+          </Button>
+        </Form>
+      </div>
     )
   }
 }
